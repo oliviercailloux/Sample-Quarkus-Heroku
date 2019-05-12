c3b1caf714fce2fab50bf3e611dbf4f127a47e66
Olivier Cailloux <olivier.cailloux@gmail.com>
2019-05-12 21:55:27 +0200

    @PersistenceContext (ignored by Weld)

--- src/main/java/io/github/oliviercailloux/y2019/jetty/ApplicationScopedCounter.java
+++ src/main/java/io/github/oliviercailloux/y2019/jetty/ApplicationScopedCounter.java
@@ -3,6 +3,12 @@ package io.github.oliviercailloux.y2019.jetty;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * From
@@ -10,10 +16,21 @@ import javax.enterprise.context.ApplicationScoped;
  */
 @ApplicationScoped
 public class ApplicationScopedCounter {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationScopedCounter.class);
 
 	private final AtomicInteger counter = new AtomicInteger();
 
+	@Inject
+	private Injected injected;
+
+	@PersistenceContext
+	EntityManager em;
+
 	public int getNumber() {
+		LOGGER.info("Injected: {}.", injected);
+		LOGGER.info("EM: {}.", em);
 		return counter.incrementAndGet();
 	}
+
 }
---
+++ src/main/java/io/github/oliviercailloux/y2019/jetty/Injected.java
@@ -0,0 +1,10 @@
+package io.github.oliviercailloux.y2019.jetty;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Injected {
+	public Injected() {
+		/** Empty. */
+	}
+}
