ae48e65b2d7816c78afdd43bb12b2b6de7217380
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2020-08-04 21:46:24 +0200

    Set up for Heroku HTTPS

--- src/main/java/io/github/oliviercailloux/jetty/App.java
+++ src/main/java/io/github/oliviercailloux/jetty/App.java
@@ -7,11 +7,16 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.ForwardedRequestCustomizer;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.jetty.JettyHttpContainer;
+import org.glassfish.jersey.server.ContainerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,11 +27,22 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		final String envPort = System.getenv("PORT");
 		final int port = envPort != null ? Integer.parseInt(envPort) : 8080;
-		final URI uri = new URI("http", null, "localhost", port, "/", null, null);
-		/** Only the scheme and port of the uri is used when creating the server! */
-		final Server server = JettyHttpContainerFactory.createServer(uri, new MyJaxRsApp(), false);
 
-		final Handler jerseyHandler = server.getHandler();
+		final JettyHttpContainer jerseyHandler = ContainerFactory.createContainer(JettyHttpContainer.class,
+				new MyJaxRsApp());
+		final Server server = new Server();
+		final HttpConfiguration config = new HttpConfiguration();
+		/**
+		 * Add support for X-Forwarded headers: thanks to
+		 * https://stackoverflow.com/a/28520946. Required because of
+		 * https://devcenter.heroku.com/articles/http-routing.
+		 */
+		config.addCustomizer(new ForwardedRequestCustomizer());
+		@SuppressWarnings("resource")
+		final ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory(config));
+		/** According to the SO (link here above), the port must be set here. */
+		connector.setPort(port);
+		server.setConnectors(new Connector[] { connector });
 
 		/**
 		 * https://www.eclipse.org/jetty/documentation/current/embedding-jetty.html#_using_handlers
@@ -44,6 +60,7 @@ public class App {
 
 		server.start();
 		final Client client = ClientBuilder.newClient();
+		final URI uri = new URI("http", null, "localhost", port, "/", null, null);
 		final WebTarget target = client.target(uri).path("counter");
 		final int result = target.request(MediaType.TEXT_PLAIN).get(Integer.class);
 		client.close();
