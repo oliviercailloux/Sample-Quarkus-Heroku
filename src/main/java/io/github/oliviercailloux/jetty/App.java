07cd9d6600dad7ccffb553573920e3c3f53452d8
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2020-08-04 21:56:55 +0200

    Ordering code

--- src/main/java/io/github/oliviercailloux/jetty/App.java
+++ src/main/java/io/github/oliviercailloux/jetty/App.java
@@ -28,43 +28,53 @@ public class App {
 		final String envPort = System.getenv("PORT");
 		final int port = envPort != null ? Integer.parseInt(envPort) : 8080;
 
-		final JettyHttpContainer jerseyHandler = ContainerFactory.createContainer(JettyHttpContainer.class,
-				new MyJaxRsApp());
 		final Server server = new Server();
-		final HttpConfiguration config = new HttpConfiguration();
-		/**
-		 * Add support for X-Forwarded headers: thanks to
-		 * https://stackoverflow.com/a/28520946. Required because of
-		 * https://devcenter.heroku.com/articles/http-routing.
-		 */
-		config.addCustomizer(new ForwardedRequestCustomizer());
-		@SuppressWarnings("resource")
-		final ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory(config));
-		/** According to the SO (link here above), the port must be set here. */
-		connector.setPort(port);
-		server.setConnectors(new Connector[] { connector });
 
-		/**
-		 * https://www.eclipse.org/jetty/documentation/current/embedding-jetty.html#_using_handlers
-		 */
-		final ResourceHandler resource_handler = new ResourceHandler();
-		resource_handler.setDirectoriesListed(true);
-		resource_handler.setWelcomeFiles(new String[] { "hello.txt" });
-		resource_handler.setResourceBase("src/main/webapp");
+		{
+			final HttpConfiguration config = new HttpConfiguration();
+			/**
+			 * Add support for X-Forwarded headers: thanks to
+			 * https://stackoverflow.com/a/28520946. Required because of
+			 * https://devcenter.heroku.com/articles/http-routing.
+			 */
+			config.addCustomizer(new ForwardedRequestCustomizer());
+			@SuppressWarnings("resource")
+			final ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory(config));
+			/** According to the SO (link here above), the port must be set here. */
+			connector.setPort(port);
+			server.setConnectors(new Connector[] { connector });
+		}
 
-		final HandlerList handlers = new HandlerList();
-		handlers.addHandler(resource_handler);
-		handlers.addHandler(jerseyHandler);
-		server.setHandler(handlers);
-		LOGGER.info("Set handler: {}.", server.getHandler());
+		{
+			/**
+			 * https://www.eclipse.org/jetty/documentation/current/embedding-jetty.html#_using_handlers
+			 */
+			final HandlerList handlers = new HandlerList();
+
+			final ResourceHandler resource_handler = new ResourceHandler();
+			resource_handler.setDirectoriesListed(true);
+			resource_handler.setWelcomeFiles(new String[] { "hello.txt" });
+			resource_handler.setResourceBase("src/main/webapp");
+
+			final JettyHttpContainer jerseyHandler = ContainerFactory.createContainer(JettyHttpContainer.class,
+					new MyJaxRsApp());
+
+			handlers.addHandler(resource_handler);
+			handlers.addHandler(jerseyHandler);
+			server.setHandler(handlers);
+			LOGGER.info("Set handler: {}.", server.getHandler());
+		}
 
 		server.start();
-		final Client client = ClientBuilder.newClient();
-		final URI uri = new URI("http", null, "localhost", port, "/", null, null);
-		final WebTarget target = client.target(uri).path("counter");
-		final int result = target.request(MediaType.TEXT_PLAIN).get(Integer.class);
-		client.close();
-		LOGGER.info("Got counter: {}.", result);
+
+		{
+			final Client client = ClientBuilder.newClient();
+			final URI uri = new URI("http", null, "localhost", port, "/", null, null);
+			final WebTarget target = client.target(uri).path("counter");
+			final int result = target.request(MediaType.TEXT_PLAIN).get(Integer.class);
+			client.close();
+			LOGGER.info("Got counter: {}.", result);
+		}
 
 		server.join();
 	}
