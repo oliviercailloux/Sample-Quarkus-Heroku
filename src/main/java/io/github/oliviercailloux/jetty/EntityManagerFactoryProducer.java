2e34127b3cf09e66f3ac261b4f5367a39900ab68
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-10-29 19:53:46 +0100

    Hibernate BeanManager ref

--- src/main/java/io/github/oliviercailloux/jetty/EntityManagerFactoryProducer.java
+++ src/main/java/io/github/oliviercailloux/jetty/EntityManagerFactoryProducer.java
@@ -1,10 +1,13 @@
 package io.github.oliviercailloux.jetty;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -15,10 +18,15 @@ import javax.persistence.Persistence;
 @ApplicationScoped
 public class EntityManagerFactoryProducer {
 
+	@Inject
+	private BeanManager beanManager;
+
 	@Produces
 	@ApplicationScoped
 	public EntityManagerFactory produceEntityManagerFactory() {
-		return Persistence.createEntityManagerFactory("PU", new HashMap<>());
+		Map<String, Object> props = new HashMap<>();
+		props.put("javax.persistence.bean.manager", beanManager);
+		return Persistence.createEntityManagerFactory("PU", props);
 	}
 
 	public void close(@Disposes EntityManagerFactory entityManagerFactory) {
