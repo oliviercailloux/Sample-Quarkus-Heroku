402623a0ea6761968cae3e63d4d236eadf422d0a
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-06-06 23:42:05 +0200

    Updt test ren resource

--- src/test/java/io/github/oliviercailloux/GreetingResourceTest.java
+++ src/test/java/io/github/oliviercailloux/GreetingResourceTest.java
@@ -1,21 +1,17 @@
 package io.github.oliviercailloux;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+	@Test
+	public void testHelloEndpoint() {
+		given().when().get("/hello").then().statusCode(200).body(is("Hello"));
+	}
 
 }
