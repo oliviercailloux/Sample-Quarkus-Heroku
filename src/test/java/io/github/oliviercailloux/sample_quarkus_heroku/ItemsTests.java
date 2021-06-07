954619ab14dae7caf3c9f5c3c58e8bb3bf89ac5b
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-06-08 00:18:14 +0200

    Impr tests items

--- src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/ItemsTests.java
+++ src/test/java/io/github/oliviercailloux/sample_quarkus_heroku/ItemsTests.java
@@ -3,14 +3,25 @@ package io.github.oliviercailloux.sample_quarkus_heroku;
 import static io.restassured.RestAssured.given;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.hamcrest.core.Is;
+import org.hamcrest.text.MatchesPattern;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class ItemsTests {
 
 	@Test
-	public void testHelloEndpoint() {
-		given().when().get("/items").then().statusCode(200);
+	public void testGet() {
+		given().when().get("/v0/items").then().statusCode(200).body(Is.is(""));
+	}
+
+	@Test
+	public void testPost() {
+		given().post("/v0/items");
+		given().when().get("/v0/items").then().statusCode(200).body(MatchesPattern.matchesPattern("MyItem dated .*"));
+		given().post("/v0/items");
+		given().when().get("/v0/items").then().statusCode(200)
+				.body(MatchesPattern.matchesPattern("MyItem dated .*\nMyItem dated .*"));
 	}
 
 }
